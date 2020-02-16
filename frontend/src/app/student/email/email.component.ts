f41230b2fe54d65ccc160f9115e478f98b26fdd6
Sebastian Erhardt <sebastian@ex-studios.net>
2020-02-15 22:20:30 -0800

    + start mobile styles 2 + finalize

--- frontend/src/app/student/email/email.component.ts
+++ frontend/src/app/student/email/email.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-email',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  submit() {
+    this.router.navigate(['/student/auth'])
   }
 
 }
